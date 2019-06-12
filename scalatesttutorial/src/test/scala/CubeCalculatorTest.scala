2ec116f93a85bab4a195ffe92efd79cf33f78f90
s06181110 <s06181110@gmail.com>
2019-06-13 02:44:10 +0900

    add test code sample

---
+++ scalatesttutorial/src/test/scala/CubeCalculatorTest.scala
@@ -0,0 +1,7 @@
+class CubeCalculatorTest extends org.scalatest.FunSuite {
+  test("CubeCalculator.cube") {
+    assert(CubeCalculator.cube(3) === 27)
+    assert(CubeCalculator.cube(2) === 8)
+    assert(CubeCalculator.cube(5) === 125)
+  }
+}
