f2f2e217574631f853373e2e9c7b8cacbf2d3279
s06181110 <s06181110@gmail.com>
2019-06-13 03:48:34 +0900

    with IntelliJ

--- scalatesttutorial/src/test/scala/CubeCalculatorTest.scala
+++ scalatesttutorial/src/test/scala/CubeCalculatorTest.scala
@@ -4,4 +4,7 @@ class CubeCalculatorTest extends org.scalatest.FunSuite {
     assert(CubeCalculator.cube(2) === 8)
     assert(CubeCalculator.cube(5) === 125)
   }
+  test("CubeCalculator.cube2") {
+    assert(CubeCalculator.cube(0) === 0)
+  }
 }
